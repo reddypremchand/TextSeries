78a6cd7c200e590d1d51613f948b84701a44cc7a
Premchand Reddy <132259855+reddypremchand@users.noreply.github.com>
2024-03-16 19:06:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 
 import { useState } from 'react';
 import Alert from './Components/Alert';
-
+import About from './Components/About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+  
+} from "react-router-dom"
 
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
@@ -20,33 +26,45 @@ function App() {
       setAlert(null);
     }, 1500);
   }
-
-  const toggleMode = () => {
+  const removeBodyClasses=()=>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-success')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-primary')
+    
+  }
+  const toggleMode = (cls) => {
+    removeBodyClasses();
+    console.log(cls);
+    document.body.classList.add('bg-'+cls)
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743'
       showAlert("Dark mode has been enabled", "success");
-      document.title = `TextUtil - Dark Mode`;
+      // document.title = `TextUtil - Dark Mode`;
     } else {
       setMode('light');
       document.body.style.backgroundColor = 'white'
       showAlert("Light mode has been enabled", "success");
-      document.title = `TextUtil - Light Mode`;
+      // document.title = `TextUtil - Light Mode`;
     }
   }
 
   return (
     <>
-      {/* <Router> */}
+       <Router> 
         <Navbar title="TextUtil" aboutText="About" mode={mode} toggleMode={toggleMode}/>
         <Alert alert={alert}/>
         <div className="container my-3">
-          {/* <Routes>
-            <Route path="/about" element={<About />} /> */}
-           <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-          {/* </Routes> */}
+           <Routes>
+            <Route path="/about" element={<About mode={mode} />} /> 
+            <Route path="/" element={ <TextForm showAlert={showAlert} heading="TextUtils- Word Counter,Charecter Counter,Download Text" mode={mode}/>}/>
+           </Routes> 
+          {/* <About mode={mode}/> */}
         </div>
-      {/* </Router> */}
+      </Router> 
     </>
   );
 }
