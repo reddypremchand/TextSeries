7a80ad009319618bd65650986a289b7b7e2d2756
Premchand Reddy <132259855+reddypremchand@users.noreply.github.com>
2024-03-16 19:11:20 +0530

    Update TextForm.js

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -21,6 +21,15 @@ function TextForm(props) {
       setText(newText);
       props.showAlert("Text is cleared!","success");
     }
+    const handleCopy = () =>{
+        console.log("I am copy");
+        var text = document.getElementById("myBox");
+        text.select();
+        text.setSelectionRange(0, 9999);
+        navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
+        props.showAlert("Copied to Clipboard!", "success");
+    }
     
       const handleDown = () => {
         // Create a new blob with the text content
@@ -45,23 +54,24 @@ function TextForm(props) {
   return (
     <>
     <div className='container' style={{color:props.mode === 'dark' ? 'white':'#042743'}}>
-        <h1>{props.heading}</h1>
+        <h1 className='mb-2'>{props.heading}</h1>
   <div class="mb-3">
-    <label for="myBox" className="form-label">Example textarea</label>
-    <textarea className="form-control" id="myBox" value={text} style={{backgroundColor:props.mode === 'dark' ? 'gray':'white',color:props.mode === 'dark' ? 'white':'#042743'}} onChange={handleOnChange} rows="8"></textarea>
+    
+    <textarea className="form-control" id="myBox" value={text} style={{backgroundColor:props.mode === 'dark' ? '#13466e':'white',color:props.mode === 'dark' ? 'white':'#042743'}} onChange={handleOnChange} rows="8"></textarea>
   </div>
-  <button onClick={handleClick} className='btn btn-primary mx-2'>Convart Uppercase</button>
-  <button onClick={handleLoClick} className='btn btn-primary mx-2'>Convart Lowercase</button>
-  <button onClick={handleClear} className='btn btn-primary mx-2'>Clear</button>
-  <button onClick={handleDown} className='btn btn-primary mx-2'>Downloard Text</button>
+  <button disabled={text.length===0} onClick={handleClick} className='btn btn-primary mx-2 my-2 '>Convart Uppercase</button>
+  <button disabled={text.length===0} onClick={handleLoClick} className='btn btn-primary mx-2 my-2'>Convart Lowercase</button>
+  <button disabled={text.length===0} onClick={handleClear} className='btn btn-primary mx-2 my-2'>Clear</button>
+  <button disabled={text.length===0} onClick={handleDown} className='btn btn-primary mx-2 my-2'>Downloard Text</button>
+  <button disabled={text.length===0} onClick={handleCopy} className='btn btn-primary mx-2 my-2'>Copy Text</button>
   </div>
 
   <div className="container my-3" style={{color:props.mode === 'dark' ? 'white':'#042743'}}>
     <h2>Your text summary</h2>
-    <p>{text.split(" ").length} words and {text.length} characters</p>
-    <p>{0.008 *text.split(" ").length }Minutes red</p>
+    <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+    <p>{0.008 *text.split(" ").filter((element)=>{return element.length!==0}).length }Minutes red</p>
     <h2>Preview</h2>
-    <p>{text.length>0?text:'Enter something in the textbox about to preview it here'}</p>
+    <p>{text.length>0?text:'Nothing to preview!'}</p>
   </div>
   </>
   )
